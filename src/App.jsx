ce9a1c421fc93ab744b4ea0c5f78e4965e3fea1f
Hafidz Siddiq <149846288+SiddiqDev043@users.noreply.github.com>
2025-06-17 19:00:35 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import React, { useEffect, useRef } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import { Home,Picture,Card,Cake,Present } from "./components"
 import './index.css'
 
 function App() {
 
+  const audioRef = useRef(null);
+  const isPlayedRef = useRef(false); // untuk mencegah double play
+
+  useEffect(() => {
+    audioRef.current = new Audio('/StillWithYou.mp3'); // dari folder public
+    audioRef.current.loop = true; // biar muter terus
+
+    const handleClick = () => {
+      if (!isPlayedRef.current && audioRef.current) {
+        audioRef.current.play().catch((err) => {
+          console.log('Gagal memutar audio:', err);
+        });
+        isPlayedRef.current = true;
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+      if (audioRef.current) {
+        audioRef.current.pause();
+        audioRef.current = null;
+      }
+    };
+  }, []);
+
+  
   return (
     <Router>
       <Routes>
